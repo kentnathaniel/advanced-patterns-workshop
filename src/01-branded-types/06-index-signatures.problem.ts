e524e0ad318423a7ad91468405e0ab3e24568044
kentnathaniel <kent.nathaniel2@gmail.com>
2024-05-03 05:19:06 +0700

    feat: initial commit

--- src/01-branded-types/06-index-signatures.problem.ts
+++ src/01-branded-types/06-index-signatures.problem.ts
@@ -1,18 +1,18 @@
-import { it } from 'vitest'
-import { Brand } from '../helpers/Brand'
-import { Equal, Expect } from '../helpers/type-utils'
+import { it } from "vitest";
+import { Brand } from "../helpers/Brand";
+import { Equal, Expect } from "../helpers/type-utils";
 
-type PostId = Brand<string, 'PostId'>
-type UserId = Brand<string, 'UserId'>
+type PostId = Brand<string, "PostId">;
+type UserId = Brand<string, "UserId">;
 
 interface User {
-  id: UserId
-  name: string
+  id: UserId;
+  name: string;
 }
 
 interface Post {
-  id: PostId
-  title: string
+  id: PostId;
+  title: string;
 }
 
 /**
@@ -23,44 +23,44 @@ interface Post {
  * two!
  */
 type DB = {
-  [postId: PostId]: Post
-  [userId: UserId]: User
-}
+  [postId: PostId]: Post;
+  [userId: UserId]: User;
+};
 
-const db: DB = {}
+const db: DB = {};
 
-it('Should let you add users and posts to the db by their id', () => {
-  const postId = 'post_1' as PostId
-  const userId = 'user_1' as UserId
+it("Should let you add users and posts to the db by their id", () => {
+  const postId = "post_1" as PostId;
+  const userId = "user_1" as UserId;
 
   db[postId] = {
     id: postId,
-    title: 'Hello world',
-  }
+    title: "Hello world",
+  };
 
   db[userId] = {
     id: userId,
-    name: 'Miles',
-  }
+    name: "Miles",
+  };
 
-  const post = db[postId]
-  const user = db[userId]
+  const post = db[postId];
+  const user = db[userId];
 
   type tests = [
     Expect<Equal<typeof post, Post>>,
     Expect<Equal<typeof user, User>>
-  ]
-})
+  ];
+});
 
-it('Should fail if you try to add a user under a post id', () => {
-  const postId = 'post_1' as PostId
-  const userId = 'user_1' as UserId
+it("Should fail if you try to add a user under a post id", () => {
+  const postId = "post_1" as PostId;
+  const userId = "user_1" as UserId;
 
   const user: User = {
     id: userId,
-    name: 'Miles',
-  }
+    name: "Miles",
+  };
 
   // @ts-expect-error
-  db[postId] = user
-})
+  db[postId] = user;
+});
